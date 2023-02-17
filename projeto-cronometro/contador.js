4ea9e2c33df4dcbcf44d4062d4073973a56367f5
Igor Monteiro <monteiroigor2022@gmail.com>
2023-02-17 15:34:16 -0300

    script cronometro

---
+++ projeto-cronometro/contador.js
@@ -0,0 +1,71 @@
+//Declarando as variáveis
+let second = 0
+let minute = 0
+let hour = 0
+let intervalo 
+let display = document.getElementById("display")
+let tempo
+let tempos = document.getElementById("tempos")
+
+//Clicou no botão start
+function start(){
+    rodar()
+    intervalo = setInterval(rodar,1000)
+}
+
+//Clicou no botão pause
+function pause(){
+    clearInterval(intervalo)
+}
+
+//Clicou no botão reset
+function reset(){
+    clearInterval(intervalo)
+    tempos.innerHTML += `<p>${tempo}</p>`
+
+    second = 0
+    minute = 0
+    hour = 0
+    display.innerHTML = `00:00:00`
+    
+}
+
+//Sistema de Contagem
+function rodar(){
+    second++
+
+    if (second == 60){
+        second = 0
+        minute++
+    }
+    if (minute == 60){
+        minute = 0
+        hour++
+    }
+    tempo = display.innerHTML = `${twoNumber(hour)}: ${twoNumber(minute)}: ${twoNumber(second)}`
+}
+
+
+//Formata os números
+function twoNumber(digito){
+    if (digito < 10){
+        return("0"+digito)
+    }
+    else{
+        return(digito)
+    }
+    
+}
+
+function history(){
+    if (tempos.style.display == "block"){
+        tempos.style.display = "none"
+    } else {
+        tempos.style.display = "block"
+    }
+}
+
+function clean(){
+   window.location.href = "index.html"
+}
+
